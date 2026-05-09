aebfb099b7325251fa8ec8def1504755045e3f89
Maksonchik <maksonchik@Maksonchiks-MacBook-Air.local>
2026-05-09 20:10:56 +0300

    update DtoArticle.cs

--- Model/Core/DTOs/DtoArticle.cs
+++ Model/Core/DTOs/DtoArticle.cs
@@ -3,11 +3,59 @@ namespace Model.Core.DTOs;
 
 public class DtoArticle
 {
-    string Title { get; set; }
-    string Text { get; set; }
-    string[] Keywords { get; set; }
-    DateTime PublishedAt { get; set; }
-    string ISSN { get; set; }
-    List<Author> Authors { get; set; }
-    ArticleType Type { get; set; }
+    public string Title { get; set; }
+    public string Text { get; set; }
+    public string[] Keywords { get; set; }
+    public DateTime PublishedAt { get; set; }
+    public string ISSN { get; set; }
+    public string[] AuthorsNames { get; set; }
+    public ArticleType Type { get; set; }
+
+    // ResearchArticle
+    public string Methodology { get; set; }
+    public string Results { get; set; }
+
+    // ReviewArticle
+    public string[] Sources { get; set; }
+    public string ReviewPeriod { get; set; }
+
+    // CaseStudy
+    public string CaseDescription { get; set; }
+    public string Conclusions { get; set; }
+
+    public DtoArticle(Article article)
+    {
+        Title = article.Title;
+        Text = article.Text;
+        Keywords = article.Keywords;
+        PublishedAt = article.PublishedAt;
+        ISSN = article.ISSN;
+        AuthorsNames = article.Authors.Select(a => a.Name).ToArray();
+        Type = article.Type;
+
+        Methodology = "";
+        Results = "";
+
+        Sources = Array.Empty<string>();
+        ReviewPeriod = "";
+
+        CaseDescription = "";
+        Conclusions = "";
+
+        if (article is ResearchArticle researchArticle)
+        {
+            Methodology = researchArticle.Methodology;
+            Results = researchArticle.Results;
+        }
+        else if (article is ReviewArticle reviewArticle)
+        {
+            Sources = reviewArticle.Sources;
+            ReviewPeriod = reviewArticle.ReviewPeriod;
+        }
+        else if (article is CaseStudy caseStudy)
+        {
+            CaseDescription = caseStudy.CaseDescription;
+            Conclusions = caseStudy.Conclusions;
+        }
+    }
 }
