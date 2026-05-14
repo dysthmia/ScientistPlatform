b1bef72489df048a1beedd3ba5b2a7326abc25bc
Maksonchik <maksonchik@Maksonchiks-MacBook-Air.local>
2026-05-14 20:19:03 +0300

    add exception for Publisher.cs

--- Model/Core/Publisher.cs
+++ Model/Core/Publisher.cs
@@ -2,25 +2,45 @@ namespace Model.Core;
 
 public class Publisher
 {
+    private List<string> _themes;
     public string Name { get; private set; }
     public double Rating { get; private set; }
-    public List<string> Themes { get; private set; }
+    public string[] Themes => _themes.ToArray();
 
     public Publisher(string name, double rating, List<string> themes)
-    {
-        if (themes == null) return;
-        
+    {      
+        ValidateName(name);
+        _themes = CheckAndCopyThemes(themes);
+
         Name = name;
         Rating = rating;
-        Themes = themes;
     } 
 
-    private bool HasTheme (string theme) => Themes.Any(t => t == theme);
     public void AddTheme (string theme)
     {
-        if (string.IsNullOrWhiteSpace(theme)) return;
-        if (HasTheme(theme)) return;
-        Themes.Add(theme);
+        if (!CheckAvailabilityTheme(theme)) _themes.Add(theme);
     }
-    public void RemoveTheme (string theme) => Themes.Remove(theme);
+    public void RemoveTheme (string theme)
+    {
+        if (CheckAvailabilityTheme(theme)) _themes.Remove(theme);
+    }
+
+    private List<string> CheckAndCopyThemes(List<string> themes)
+    {
+        List<string> checked_themes = new List<string>();
+        foreach(var theme in themes) AddTheme(theme);
+        return checked_themes;
+    }
+    private bool CheckAvailabilityTheme (string theme)
+    {
+        if (string.IsNullOrWhiteSpace(theme))
+            throw new ArgumentException(nameof(theme), "Тема не должна быть пустой");
+        
+        return _themes.Any(t => t == theme);
+    }
+    private void ValidateName (string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException(nameof(name), "Имя Publisher не должно быть пустым");
+    }
 }
