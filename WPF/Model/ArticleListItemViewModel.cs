86a29912a2350baefc18f5d6dd466cdd124175ce
triplesixblast <astracelebrities@gmail.com>
2026-05-28 12:02:10 +0300

    article now opens on click

--- WPF/Model/ArticleListItemViewModel.cs
+++ WPF/Model/ArticleListItemViewModel.cs
@@ -1,16 +1,28 @@
+using System;
 using System.Globalization;
 using System.Linq;
+using CommunityToolkit.Mvvm.Input;
 using Model.Core;
+
 namespace WPF.ViewModels;
+
 public class ArticleListItemViewModel
 {
+    private readonly Article _article;
+    private readonly Action<Article> _onOpen;
+
     public string Title { get; }
     public string PublishedAt { get; }
     public string Authors { get; }
-    public ArticleListItemViewModel(Article article)
+    public IRelayCommand OpenCommand { get; }
+
+    public ArticleListItemViewModel(Article article, Action<Article> onOpen)
     {
+        _article = article;
+        _onOpen = onOpen;
         Title = article.Title;
         PublishedAt = article.PublishedAt.ToString("dd MMMM yyyy", CultureInfo.GetCultureInfo("ru-RU"));
-        Authors = string.Join(", ", article.Authors.Select(author => author.Name));
+        Authors = article.JoinAuthors();
+        OpenCommand = new RelayCommand(() => _onOpen(_article));
     }
 }
